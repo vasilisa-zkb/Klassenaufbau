5942468ecaa35c659703327335e747a620c94c42
vasilisa <vasilisageigerr@gmail.com>
2025-10-28 10:28:41 +0100

    Song

--- src/main/java/exercise1/Main.java
+++ src/main/java/exercise1/Main.java
@@ -5,9 +5,8 @@ public class Main {
     public static void main(String[] args) {
         //Main Method
       Song song = new Song("ITALODISCO", 199, "The Kolors");
-song.songTime();
-        System.out.println();
 
+        System.out.println();
 
         System.out.println(song);
     }
